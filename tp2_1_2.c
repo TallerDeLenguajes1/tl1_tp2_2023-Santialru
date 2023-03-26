c6267f93d644104006ad81c729f3e890c9adb180
Santiago <alvarezrubiosanti@gmail.com>
2023-03-25 23:51:55 -0300

    ejercicio con recorrido por puntero

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -1,1 +1,24 @@
-rama2
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define N 20
+
+int main(){
+    int i;
+    double vt[N];
+    double *punt;
+
+    punt = &vt[0];
+    srand(time(NULL));
+    punt = &vt[0];
+
+    for(i = 0;i<N; i++)
+    {
+    vt[i]=1+rand()%100;
+    printf("%f ", *punt);
+    punt++;
+    }
+    
+
+    return 0;
+}
